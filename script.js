0d021ec02e208ef927c9967b0f0118c842f5fb46
Soumyadeep Guha <soumyadeepguha.54ds@gmail.com>
2025-07-19 16:08:07 +0530

    Functionality added

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Script is running :)");
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Write the task first.");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+}
